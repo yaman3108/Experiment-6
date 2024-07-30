72546ffaf2969d73d5705afba78701e4191c9b2f
yaman3108 <133077812+yaman3108@users.noreply.github.com>
2024-07-30 15:16:38 +0530

    Create EXP5.1.cpp

---
+++ EXP5.1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+//Name: Yaman Hasan Ansari PRN:23070123155
+
+int main()
+{
+  int num;
+  cout<<"Enter a positive number: ";
+  cin>>num;
+  if(num==0){
+    cout<<num<<" invalid  input, please enter a positive integer"
+}
