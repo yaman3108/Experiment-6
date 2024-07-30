1cc9be5d740295c32647d5bd4818d2c4304d677a
yaman3108 <133077812+yaman3108@users.noreply.github.com>
2024-07-30 16:01:23 +0530

    Update EXP5.1.cpp

--- EXP5.1.cpp
+++ EXP5.1.cpp
@@ -2,12 +2,17 @@
 using namespace std;
 
 //Name: Yaman Hasan Ansari PRN:23070123155
-
-int main()
-{
-  int num;
-  cout<<"Enter a positive number: ";
-  cin>>num;
-  if(num==0){
-    cout<<num<<" invalid  input, please enter a positive integer"
-}
+int main() {
+    int  num;
+    cout<<"Enter positive number: ";
+    cin>>num;
+    if(num==0) {
+        cout<<num<<" is zero"<<endl;
+    }
+    else if(num%2!=0){
+        cout<<num<<" is odd"<<endl;
+    }
+    else {
+        cout<<num<<" is even"<<endl;
+    return 0;
+    }
